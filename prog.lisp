e1aa50e48f4045c41e350021e125f4acd4893cc9
Settarov Ernest <39336766+TIR13@users.noreply.github.com>
2020-04-08 13:53:31 +0300

    Update prog.lisp

--- prog.lisp
+++ prog.lisp
@@ -280,3 +280,36 @@
 (del-all 'человек)
 (print (symbol-plist 'человек))
 (write-line "")
+
+
+
+;48. Функция GET возвращает в качестве результата NIL
+;в том случае, если у символа нет данного свойства, либо
+;если значением этого свойства является NIL.
+;Следовательно, функцией GET нельзя проверить, есть ли некоторое свойство в
+;списке свойств. Напишите предикат (ИМЕЕТ-СВОЙСТВО символ свойство),
+;который проверяет, обладает ли символ данным свойством.
+
+(defun prov (symb sv)
+	(if(null (get symb sv)) nil t)
+)
+(write-line "Задача 48")
+;;; Test 1
+(setf (get 'cars 'color) 'red)
+(setf (get 'cars 'тип) 'седан)
+
+
+;;; Test 1
+(print "Test 1")
+(print (symbol-plist 'cars))
+(print "(prov cars color)")
+(print (prov 'cars 'color))
+(write-line "")
+
+;;; Test 2
+(print "Test 2")
+(print (symbol-plist 'cars))
+(print "(prov cars col)")
+(print (prov 'cars 'col))
+(write-line "")
+
