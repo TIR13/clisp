0001c82cefcdc8d22886a54571715cdff575ddba
Settarov Ernest <39336766+TIR13@users.noreply.github.com>
2020-05-27 23:25:15 +0300

    Update hask.hs

--- hask.hs
+++ hask.hs
@@ -1,3 +1,54 @@
+
+--12. Определите функцию, разбивающий список (a b с d...) на пары ((а b) (с d)...).
+
+main = do
+    print $ zip ["1","2","3","4","5"] ["a","5","5","5","5"]  
+
+
+--24. Написать функция Разность, формирующая разность двух множеств.
+
+delete                  :: Eq a => a -> [a] -> [a]
+delete                  =  deleteBy (==)
+
+deleteBy                :: (a -> a -> Bool) -> a -> [a] -> [a]
+deleteBy eq x []        = []
+deleteBy eq x (y:ys)    = if x `eq` y then ys else y : deleteBy eq x ys
+
+(\\) :: Eq a => [a] -> [a] -> [a]
+(\\) =  foldl (flip delete)
+main = do
+    print $ [1, 2, 3] \\ [4]
+    print $ [1, 2, 3] \\ [3]
+
+
+---Написать генератор деррева 
+
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+              deriving (Show, Eq)
+              
+leaf x = Branch x Empty Empty
+
+cbalTree :: Int -> [Tree Int]
+cbalTree 0 = [Empty]
+cbalTree n = let (q, r) = (n - 1) `quotRem` 2
+    in [Branch n left right |   
+                                i     <- [q .. q + r],
+                                left  <- cbalTree i,
+                                right <- cbalTree (n - i - 1)]
+                                                         
+main = do
+print $ cbalTree 4
+
+
+
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
 --2. Определите функци, функцию, заменяющую в исходном списке все вхождения заданного значения другим.
 
 m lst from to = map 
