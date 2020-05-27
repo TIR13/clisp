9dd73f85183ef5926870bead6ff923c2566efac5
Settarov Ernest <39336766+TIR13@users.noreply.github.com>
2020-05-27 17:37:45 +0300

    Update ready_lang.lisp

--- ready_lang.lisp
+++ ready_lang.lisp
@@ -1,28 +1,19 @@
 (require "/lang.lisp")
 
-
-(print "Transp: ((1 2 3) (4 5 6) (7 8 9))")
-(print (Transp '((1 2 3) (4 5 6) (7 8 9))))
-
-(write-line "")
-(print "Maxx: ((1 2 3) (4 5 6) (7 8 9))")
-(print (Maxx '((1 2 3) (4 5 6) (7 8 9))))
-
-(write-line "")
-(print "Minn: ((1 2 3) (4 5 6) (7 8 9))")
-(print (Minn '((1 2 3) (4 5 6) (7 8 9))))
-
-(write-line "")
-(print "Determ: ((3 5 1) (4 5 6) (7 8 9))")
-(print (Determ '((3 5 1) (4 5 6) (7 8 9))))
-
-(write-line "")
-(print "Sum: ((1 2 3) (4 5 6) (7 8 9))")
-(print (Sum '((1 2 3) (4 5 6) (7 8 9)) '((1 2 3) (4 5 6) (7 8 9))))
-
-(write-line "")
-(print "Multiplay: ((1 2 3) (4 5 6) (7 8 9))")
-(print (Multiplay '((1 2 3) (4 5 6) (7 8 9)) '((1 2 3) (4 5 6) (7 8 9))))
-
-
+(Matrix 
+    M1 = ((3 5 1) (4 5 6) (7 8 9))
+    M2 = ((1 2 4) (4 5 6) (7 8 9)) 
+    *
+    +
+    max 
+    min 
+    t
+    M1 = ((4 5 1) (4 5 6) (7 8 9))
+    *
+    +
+    max 
+    min 
+    t
+    @
+)
 
